3bcf02f03745b54e97e420f12a9f059cdcae4fad
Ekemini Felix <felixekemini9@gmail.com>
2021-06-18 23:07:49 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two -digits numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int x;
+
+x= 'a';
+
+for (; x <= 'z'; x++)
+{
+	putchar(x);
+
+}
+
+return (0);
+
+
+
+}
+
