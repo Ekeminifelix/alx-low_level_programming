4804da81221f6f6d586271f2c2261996af1988f1
Ekemini Felix <felixekemini9@gmail.com>
2021-06-19 11:01:37 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two -digits numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int x;
+
+x = '0';
+
+for (; x <= '9'; x++)
+{
+	putchar(x);
+
+}
+putchar('\n');
+
+return (0);
+
+
+
+}
+
