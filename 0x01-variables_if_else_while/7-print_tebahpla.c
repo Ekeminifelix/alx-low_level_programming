59f1b8c7a3c4b29ae8bbf475e3e53e4b177b4978
Ekemini Felix <felixekemini9@gmail.com>
2021-06-19 11:37:18 +0000

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two -digits numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int x;
+
+x = 'z';
+
+for (; x >= 'a'; x--)
+{
+
+
+	putchar(x);
+
+}
+putchar('\n');
+
+return (0);
+
+
+
+}
+
