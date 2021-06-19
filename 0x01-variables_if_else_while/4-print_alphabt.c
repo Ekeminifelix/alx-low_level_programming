68ab0d9bcdf0c353e4e8df916428c17a812358bb
Ekemini Felix <felixekemini9@gmail.com>
2021-06-19 08:00:25 +0000

    ignore e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two -digits numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int x;
+
+x = 'a';
+
+while (!('e' || 'q'))
+{
+	putchar(x);
+	x++;
+}
+putchar('\n');
+
+return (0);
+
+
+
+}
