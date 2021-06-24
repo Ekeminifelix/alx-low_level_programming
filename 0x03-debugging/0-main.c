e9b2fffb2a5938d2278a1a6af52b08a2b7984adf
Ekemini Felix <felixekemini9@gmail.com>
2021-06-24 08:17:29 +0000

    positive or nagetive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* main - tests function for signs
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -9,7 +9,8 @@ int main(void)
 {
 int i;
 
-i = 0;
+i = 98;
 positive_or_negative(i);
+
 return (0);
 }
