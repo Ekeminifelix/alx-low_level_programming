69e3a1f8c78680a7d2c8e578225da91d7cc45b41
Ekemini Felix <felixekemini9@gmail.com>
2021-06-28 15:54:10 +0000

    function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_puts - function to print a string and a new line to stdout
+ *str:  holds the value of the string
+ *Return: returns length of string
+ */
+
+void _puts(char *str);
+{
+
+
+puts(str);
+
+
+
+}
