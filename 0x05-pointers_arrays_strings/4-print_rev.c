ed0367267fb58284d3d6585d12ae3915b27136ec
Ekemini Felix <felixekemini9@gmail.com>
2021-06-28 16:26:51 +0000

    function that prints a strong, in inverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - used to print a string in reverse order
+ * @s: a pointer holding a character datatype
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+int i, c = 0;
+for (i = 0; *(s + i) != '\0'; i++)
+{
+c++;
+}
+for (i = c - 1; i >= 0; i--)
+{
+
+_putchar(i);
+}
+_putchar('\n');
+}
