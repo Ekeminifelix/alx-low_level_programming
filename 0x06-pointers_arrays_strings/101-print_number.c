ee754955104ee64da46ae0d099405a72dd6c0095
Ekemini Felix <felixekemini9@gmail.com>
2021-07-02 20:57:17 +0000

    function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,34 @@
-#include <stdio.h>
 #include "holberton.h"
+void print_unsigned_int(unsigned int n);
 /**
- * print_number - prints a number
- * @n: Input number
+ * print_number - print number n with putchar
+ *
+ * @n: number to print
+ *
+ * Return: always void
  */
-
 void print_number(int n)
 {
-	int res, temp, expo;
-
-	expo = 1;
-/*Check negatives*/
-	if (n >= 0)
-		res = n * -1;
-	else
-	{
-		res = n;
-		_putchar('-');
-	}
-
-/*Initialize exponent variable*/
-	temp = res;
-	while (temp <= -10)
-	{
-		expo *= 10;
-		temp /= 10;
-	}
-/*Main */
-	while (expo >= 1)
-	{
-		_putchar(((res / expo) % 10) * -1 + '0');
-		expo /= 10;
-	}
+if (n < 0)
+{
+_putchar('-');
+print_unsigned_int(-(unsigned int)n);
+}
+else
+print_unsigned_int(n);
+}
+/**
+ * print_unsigned_int - prints an unsigned integer
+ *
+ * @n: an unsigned integer to print
+ *
+ * Return: always void
+ */
+void print_unsigned_int(unsigned int n)
+{
+if (n / 10 != 0)
+{
+print_unsigned_int(n / 10);
+}
+_putchar((n % 10) + '0');
 }
