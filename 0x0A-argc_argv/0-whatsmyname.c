8b4b53074d2beeb08afe82f60e5524a8456e7e56
Ekemini Felix <felixekemini9@gmail.com>
2021-07-12 13:01:16 +0000

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,12 +6,15 @@
  *return: o
  */
 int main(int argc, char *argv[])/*command line argurments*/
+
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
+
 	{
 		printf("%s ", argv[i]);/*printing the string to stdout*/
 	}
 	printf("\n");/*printing a new line*/
-	return(0);
+	return (0);
 }
