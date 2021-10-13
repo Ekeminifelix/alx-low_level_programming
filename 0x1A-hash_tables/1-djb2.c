d221e955931149458536336381787fc7f0471963
Ekemini Felix <felixekemini9@gmail.com>
2021-10-13 19:12:41 +0000

    hash table files

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,9 @@
-#include "hash_tables.h"
-
 /**
- *  * hash_djb2 - implementation of the djb2 algorithm
- *   * @str: string used to generate hash value
- *    *
- *     * Return: hash value
- *      */
+ * hash_djb2 - Hash function, takes a string and turns it into a hash number
+ * @str: the string, a constant value unrepeated in the hash table
+ *
+ * Return: The hash number
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -16,5 +14,5 @@ unsigned long int hash_djb2(const unsigned char *str)
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-        return (hash);
+	return (hash);
 }
